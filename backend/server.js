82c83a9bd1efb62f7b812640bbe4cce4a3ddb4b7
Anshika Sharma <211B054@juetguna.in>
2025-11-19 17:01:45 +0530

    Update backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -118,7 +118,8 @@ app.post("/api/contact", async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: parseInt(process.env.SMTP_PORT),
-      secure: false,
+secure: process.env.SMTP_PORT == "465" ? true : false,
+
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
